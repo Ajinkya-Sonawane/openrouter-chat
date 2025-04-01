2b80508782956e9d47ef5165a7286bdd7eb03375
Ajinkya-Sonawane <ajinkya.sonawane7897@gmail.com>
2025-03-31 23:15:13 -0400

    Add LICENSE

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/**
+ * OpenRouterChat - A WhatsApp-style chat application for conversing with AI models
+ * through the OpenRouter API.
+ * 
+ * Copyright (c) 2023-2024 OpenRouterChat Contributors
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+// ... rest of the file 
